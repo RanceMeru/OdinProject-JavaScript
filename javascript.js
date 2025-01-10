78871899521312edf98a693463d4097c4e2f2753
RanceMeru <johgu.829@gmail.com>
2025-01-10 11:21:39 -0800

    Javascript made a new dialog
    the console will display a message but instead of in the console it would be better to make it as an alertmessage.

--- javascript.js
+++ javascript.js
@@ -1,9 +1,13 @@
 console.log("Hello World");
-
+console.log("press 1 for rock"/n+ "press 2 for paper"
+    /n+ "press 3 for scissors");
 //rock 1 etc
 //if else or if statements when it goes through everything
 // not equal to 1 2 or 3 it will correct the user and loop back to the start
 
+let rock = 1;
+let paper = 2;
+let scissors = 3;
 function getComputerChoice(){
 
     return Math.random();
