5649c7893d066f7d40489571f8eb9ee9fec625d0
RanceMeru <johgu.829@gmail.com>
2025-01-10 10:49:12 -0800

    Javascript new requirements fufilled
    I followed the 1st and 2nd steps for the odin project assignment to create  a function called getComputerChoice and to return a string rock paper or scissors and comments that will provide more hints to help the process.

--- javascript.js
+++ javascript.js
@@ -1,43 +1,10 @@
-let name = "John";
-let surname = "Doe";
+console.log("Hello World");
 
-console.log(name);
+//rock 1 etc
+//if else or if statements when it goes through everything
+// not equal to 1 2 or 3 it will correct the user and loop back to the start
 
-console.log(surname);
+function getComputerChoice(){
 
-let age = 11;
-console.log(age); // outputs 11 to the console
-
-age = 54;
- 
-console.log(age); // what will be output now?
-
-const pi = 3.14;
-
-
-console.log(pi); // What will be output?
-
-
-console.log("this is the assignment segment");
-console.log(50+70);
-
-console.log(7+7+4+6+9);
-
-console.log((4+6+9)/77);//should display 0.24675
-
-let a = 10;
-
-console.log(a);
-
-console.log(9*a);
-
-let b = 7*a;
-
-console.log(b);
-
-const max = 57;
-const actual = max - 13;
-const percentage = actual / max;
-
-console.log(percentage); // This will output approximately 0.7719
-
+    return Math.random();
+}
