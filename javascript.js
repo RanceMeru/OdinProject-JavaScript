d5d6b61c27127c882a46aa4fbdac2dc5c1456864
RanceMeru <johgu.829@gmail.com>
2025-01-10 09:16:43 -0800

    Calculator javascript app for the The Odin Project
    I am making and completing the the project and its requirements for the odin project by setting up a basic html document and javascript file.

---
+++ javascript.js
@@ -0,0 +1,43 @@
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+
+console.log(surname);
+
+let age = 11;
+console.log(age); // outputs 11 to the console
+
+age = 54;
+ 
+console.log(age); // what will be output now?
+
+const pi = 3.14;
+
+
+console.log(pi); // What will be output?
+
+
+console.log("this is the assignment segment");
+console.log(50+70);
+
+console.log(7+7+4+6+9);
+
+console.log((4+6+9)/77);//should display 0.24675
+
+let a = 10;
+
+console.log(a);
+
+console.log(9*a);
+
+let b = 7*a;
+
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+
+console.log(percentage); // This will output approximately 0.7719
+
