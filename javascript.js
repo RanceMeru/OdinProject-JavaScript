269d26087b715bcc21fa2907cf3d5d2f2f58e06d
RanceMeru <johgu.829@gmail.com>
2025-01-13 13:05:17 -0800

    Javascript code organizations
    Changed the large space  between the code functions to it is closer and comments removed due to redundancy.

--- javascript.js
+++ javascript.js
@@ -20,7 +20,7 @@ function getComputerChoice(){
       }
       //if less than 2 make 1
       //if greater than or equal to 2 but less than 3 make 2
-/*  
+
 
     if (input === "rock" || input === "paper" || input === "scissors") {
         alert("You entered: " + input);
@@ -30,41 +30,11 @@ function getComputerChoice(){
         alert("You have to put rock, paper, or scissors");
     }
     return Math.random()
- */   
+   
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 //step 3
 function getHumanChoice(input) {
   switch(input) {
@@ -109,7 +79,7 @@ if (humanChoice) {
 } else {
   alert("Invalid input. Please enter 1, 2, or 3.");
 }
-*/
+
 
 
 //Step 5
@@ -125,14 +95,8 @@ const computerSelection = getComputerChoice();
 playRound(humanSelection, computerSelection);
 
 
-//have math.random only choose numbers from 1 to 3
-//rock 1 etc
-//if else or if statements when it goes through everything
-// not equal to 1 2 or 3 it will correct the user and loop back to the start
-
 //let choices = rock = 1, paper =2, scissors =3;
 
 
 
 
-// can make the 1 2 3 have a triangle interaction so that random number < 3 etc
