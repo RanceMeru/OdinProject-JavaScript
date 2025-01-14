e44794ca0aa8056e566b76be4d76adc843a974b2
RanceMeru <johgu.829@gmail.com>
2025-01-14 10:59:26 -0800

    Javascript comments and code moves
    I made comments to keep track of what is being used to make it easier to follow and what can be moved or changed.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,32 @@
-console.log("Hello World");
-//Step 4
+
+//Step 4 tracks the human's score
 let humanScore = 0;
 let computerScore = 0;
-function playGame(){
 
+//this how the game starts and to be called to start the game again after it has finished
+function playGame(){
+//step 6
+//makes a prompt that displays a message and takes inn input
   let input = prompt("Press 1 for rock\nPress 2 for paper\nPress 3 for scissors");
 
+  function playRound(humanChoice, computerChoice) {
+    return humanChoice.toLowerCase() === computerChoice.toLowerCase();
+  }
 
+
+  //step 3
+function getHumanChoice(input) {
+  switch(input) {
+      case "1":
+          return "Rock";
+      case "2":
+          return "Paper";
+      case "3":
+          return "Scissors";
+      default:
+          return null;
+  }
+}
 
 
 //making a function to let the computer make a choice of which number to display 1,2 or 3
@@ -25,19 +45,7 @@ function getComputerChoice(){
     }      //if less than 2 make 1
       //if greater than or equal to 2 but less than 3 make 2
 
-//step 3
-function getHumanChoice(input) {
-  switch(input) {
-      case "1":
-          return "Rock";
-      case "2":
-          return "Paper";
-      case "3":
-          return "Scissors";
-      default:
-          return null;
-  }
-}
+
 
 alert("the bot chose " + getComputerChoice());
 //change this to the top so that the player goes first 
@@ -57,11 +65,14 @@ function determineWinner(humanChoice, computerChoice) {
   }
   return "Computer";
 }
-
+//when this is changed the game is cut short but together it is fine but does not function as intended
 const humanChoice = getHumanChoice(input);
+
 const computerChoice = getComputerChoice();
 
+console.log("logical");
 
+//this is where the bot chooses a different option then what was said in the previous dialog
 if (humanChoice) {
     
     alert("You chose: " + humanChoice + "\nThe computer chose: " + computerChoice);
@@ -83,10 +94,9 @@ if (humanChoice) {
     alert("Invalid input. Please enter 1, 2, or 3.");
 }
 console.log("track score");
-function playRound(humanChoice, computerChoice) {
-  return humanChoice.toLowerCase() === computerChoice.toLowerCase();
-}
 
+
+//all below exceot function call was removed
 const humanSelection = getHumanChoice(input);
 const computerSelection = getComputerChoice();
 
